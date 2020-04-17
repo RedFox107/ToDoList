a2f95ad4a5aa248216c1a732caba1d3356a642bd
RedFox107 <e.ratnikov31@gmail.com>
2020-04-17 15:58:16 +0300

    Updates

---
+++ precache-manifest.e4e6124a7dc8f8e74396a97d083005fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ef8ea4d1ecc90dfbbc1fb09469114d",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "67ce30e61020550157c8",
+    "url": "/ToDoList/static/css/main.2d1b1f1a.chunk.css"
+  },
+  {
+    "revision": "45b501eeab71810cfaaf",
+    "url": "/ToDoList/static/js/2.30b036ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.30b036ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ce30e61020550157c8",
+    "url": "/ToDoList/static/js/main.019f3c40.chunk.js"
+  },
+  {
+    "revision": "db220d7933a9ff3a18d0",
+    "url": "/ToDoList/static/js/runtime-main.195abb9d.js"
+  }
+]);
